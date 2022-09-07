64fe82e15b8d249230baa6fb5311c872b505838b
crystal1229d <crystal1229d@naver.com>
2022-09-08 00:30:23 +0900

    create project & init setting (1) creat project using 'vite' (2) install router (3) add basic pages

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+
+import App from './app'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'), 
+)
