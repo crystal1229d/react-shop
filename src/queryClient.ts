60e41104e5ed0c0cf5da493304ffe185a01e7f62
crystal1229d <crystal1229d@naver.com>
2022-09-09 16:41:44 +0900

    add GNB (1) create Gnb page & SCSS file for it (2) add Gnb to app.tsx

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -15,7 +15,7 @@ export const getClient = (() => {
             defaultOptions: {
                 queries: {
                     cacheTime: 1000 * 60 * 60 * 24, 
-                    staleTime: 1000, 
+                    staleTime: 1000 * 60, 
                     refetchOnMount: false, 
                     refetchOnReconnect: false, 
                     refetchOnWindowFocus: false, 
