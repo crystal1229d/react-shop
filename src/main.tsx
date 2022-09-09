65c0eaa63fadcb3bdbf2dc444969f56372c56ff8
crystal1229d <crystal1229d@naver.com>
2022-09-09 21:13:52 +0900

    add MSW (mock-service-worker) & adapt graphQL (1) install MSW & graphQL (2) create graphql products types & quries (3) create mock browser (4) change from restFetcher to graphqlFetcher

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
 import './scss/index.scss'
 import App from './app'
+import { worker } from './mocks/browser'
 
+if (import.meta.env.DEV) {
+  worker.start()
+}
 
 ReactDOM.render(
   <React.StrictMode>
